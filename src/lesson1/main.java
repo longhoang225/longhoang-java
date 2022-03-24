f2a3d4fcef10d357841d1efec9cd2afa683fa7d9
longhoang225 <100844822+longhoang225@users.noreply.github.com>
2022-03-24 14:11:49 +0700

    udatemoi

---
+++ src/lesson1/main.java
@@ -0,0 +1,35 @@
+package lesson1;
+
+public class main {
+
+    public static void main(String[] args){
+//    char a = 'a';
+//    char b = 'b';
+//    int c = a + b;
+//    int d = a - b;
+//    int e = b - a;
+//    int f = a * b;
+//    int g = a / b;
+//    int h = b / a;
+//
+//    System.out.println(c);
+//    System.out.println(d);
+//    System.out.println(e);
+//    System.out.println(f);
+//    System.out.println(g);
+//    System.out.println(h);
+
+        int i=0;
+
+        int a = i++*2;
+        System.out.println(a);
+
+        i=0;
+
+        int  b = ++i * 2;
+        System.out.println(b);
+
+
+
+    }
+}
